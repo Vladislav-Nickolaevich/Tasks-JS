d4e7cfe8ec48cafaf81f066c6fd20dba3b340309
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2023-02-14 11:53:56 +0300

    Solved sumOfMinimums task

--- tasks.js
+++ tasks.js
@@ -1522,3 +1522,10 @@ function smash (words) {
   return words.join(' ');
 }
 // console.log(smash(['hello', 'world', 'this', 'is', 'great']));
+
+
+function sumOfMinimums(arr) {
+  return arr.map(el => el.sort((a, b) => a > b? 1: -1)[0]).reduce((c, i) => c + i, 0);
+}
+
+// console.log(sumOfMinimums( [[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]));
