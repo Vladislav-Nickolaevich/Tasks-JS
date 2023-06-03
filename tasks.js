21035929ab12f16a9cd6aa740aed7cd1517b2630
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2023-06-03 11:57:58 +0300

    Solved twoOldestAges task

--- tasks.js
+++ tasks.js
@@ -1953,3 +1953,9 @@ function toAcronym(inp){
 }
 
 // console.log(toAcronym("hyper text markup language"));
+
+
+function twoOldestAges(ages){
+    return ages.sort((a,b) => a - b).slice(ages.length - 2);
+}
+// console.log(twoOldestAges([1,5,87,45,8,8]));
