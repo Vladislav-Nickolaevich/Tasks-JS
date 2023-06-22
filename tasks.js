ca7d860cece697afc387e69e3d11ba20381c0ad4
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2023-06-22 09:04:35 +0300

    Solved peopleWithAgeDrink task

--- tasks.js
+++ tasks.js
@@ -2108,3 +2108,14 @@ function getChar(c){
 }
 
 // console.log(getChar(55));
+
+function peopleWithAgeDrink(old) {
+  return old <= 13? 
+          "drink toddy":
+          old <= 17?
+          "drink coke":
+          old <= 20?
+          "drink beer":
+          "drink whisky";
+}
+// console.log(peopleWithAgeDrink(22));
