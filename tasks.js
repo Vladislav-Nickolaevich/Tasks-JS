28dd590174f0e125f0597311cee4a2e240f2ff4d
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2024-03-07 08:50:31 +0300

    Solved logs task

--- tasks.js
+++ tasks.js
@@ -2865,3 +2865,11 @@ function multiply(number){
 // console.log( multiply(200));
 // console.log( multiply(0));
 // console.log( multiply(-2));
+
+
+function logs(x , a, b){
+  return (Math.log(a) / Math.log(x)) + (Math.log(b) / Math.log(x))
+}
+
+// console.log(logs(10, 2, 3));
+// console.log(logs(5, 2, 3));
