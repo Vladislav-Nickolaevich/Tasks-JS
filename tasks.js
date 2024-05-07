e9b0915d481e1a297fe1d4f1e1addd2da3f4b872
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2024-05-07 14:30:52 +0300

    Solved leo task

--- tasks.js
+++ tasks.js
@@ -2948,3 +2948,21 @@ function NameMe(first, last) {
 // console.log(n.firstName);
 // console.log(n.lastName);
 // console.log(n.name);
+
+function leo(oscar){
+  if(oscar === 88) {
+    return "Leo finally won the oscar! Leo is happy"
+  }
+  if(oscar === 86){
+    return "Not even for Wolf of wallstreet?!"
+  }
+  if(oscar !== 88 && oscar !== 86 && oscar < 88){
+    return  "When will you give Leo an Oscar?"
+  }
+  if(oscar > 88){
+    return "Leo got one already!"
+  }
+}
+
+// console.log(leo(89));
+// console.log(leo(88));
